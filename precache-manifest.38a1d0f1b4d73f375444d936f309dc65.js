df5ddd31446cdbdab3a2c011dee50d812fd655fe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-08 23:58:33 +0000

    Deploy eperedo/travis-tutorial to github.com/eperedo/travis-tutorial.git:gh-pages

---
+++ precache-manifest.38a1d0f1b4d73f375444d936f309dc65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60770d4d57e315bba296a9801e2d3640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd5573f191e4649e5dac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82b54dbb41befbc1c798",
+    "url": "/static/js/2.39ac3e47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39ac3e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5573f191e4649e5dac",
+    "url": "/static/js/main.3b1db544.chunk.js"
+  },
+  {
+    "revision": "f842cd636c09885771d3",
+    "url": "/static/js/runtime-main.0fdd964d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
